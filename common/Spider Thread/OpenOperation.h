aacdf50915e14c53c94132115c83257c991667df
Aidan Wood <fenway1000@gmail.com>
2012-12-06 16:04:15 -0800

    added Spider Thread Folder, added OpenOperation.h

---
+++ common/Spider Thread/OpenOperation.h
@@ -0,0 +1,32 @@
+#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTServo,  none)
+#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
+#pragma config(Sensor, S2,     LimitSwitches,  sensorI2CCustom9V)
+#pragma config(Sensor, S3,     IRS1,           sensorI2CCustom)
+#pragma config(Sensor, S4,     ,               sensorTouch)
+#pragma config(Motor,  mtr_S1_C1_1,     motorD,        tmotorTetrix, PIDControl, encoder)
+#pragma config(Motor,  mtr_S1_C1_2,     motorE,        tmotorTetrix, PIDControl, encoder)
+#pragma config(Motor,  mtr_S1_C2_1,     motorF,        tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C2_2,     FourBar,       tmotorTetrix, openLoop)
+#pragma config(Servo,  srvo_S1_C3_1,    servo1,               tServoNone)
+#pragma config(Servo,  srvo_S1_C3_2,    servo2,               tServoNone)
+#pragma config(Servo,  srvo_S1_C3_3,    servo3,               tServoNone)
+#pragma config(Servo,  srvo_S1_C3_4,    servo4,               tServoNone)
+#pragma config(Servo,  srvo_S1_C3_5,    servo5,               tServoNone)
+#pragma config(Servo,  srvo_S1_C3_6,    servo6,               tServoNone)
+
+void OpenOperation(Operation)
+ {
+   if(Operation=="Move")
+   {
+     motorD[30];
+     motorE[30];
+ 	 }
+ }
+
+
+
+task main()
+{
+
+
+}
